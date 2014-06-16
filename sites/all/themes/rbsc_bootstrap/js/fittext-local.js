41cda5d2f0157de882d79c3ebcc060487ae060a2
Shaun Ellis <shaun@sdellis.com>
2014-06-16 13:21:21 -0400

    added gallery views for exhibitions

--- sites/all/themes/rbsc_bootstrap/js/fittext-local.js
+++ sites/all/themes/rbsc_bootstrap/js/fittext-local.js
@@ -13,6 +13,8 @@ jQuery( document ).ready(function() {
   		jQuery( this ).css("background-color", randomColor({hue: 'blue'}));
 	});
 
+	jQuery( ".thumbnails li" ).removeClass( "span3" ).addClass( "col-xs-6 col-md-3" );
+
 	jQuery( "form#nav-search" ).on( "submit", function( event ) {
   		event.preventDefault();
   		window.location.href = "/find/all/" + encodeURIComponent(jQuery( "#nav-search-input" ).val());
