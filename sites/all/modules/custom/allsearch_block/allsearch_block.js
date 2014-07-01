5a6a03edf01ceacce26870787aad40833b6edc92
Kevin Reiss <kevin.reiss@gmail.com>
2014-07-01 13:32:16 -0400

    add all search implementation

--- sites/all/modules/custom/allsearch_block/allsearch_block.js
+++ sites/all/modules/custom/allsearch_block/allsearch_block.js
@@ -8,8 +8,8 @@
      
      $('#allsearch-block-form').submit(function() {
     	if ($(this).find("input:first").val() == "" || $(this).find("input:first").val() == Drupal.settings.allsearch_block.all_search_hint) {
-				if($('#allsearch-error').length == 0) {
-		 			$("<div id='messages' class='grid-12'><div id='allsearch-error' class='messages error'><h2 class='element-invisible'>Error message</h2>"+Drupal.settings.allsearch_block.all_search_error+"</div></div>").insertBefore('#region-content');
+                if($('#allsearch-error').length == 0) {
+		 			$("<div id='messages' class='grid-12'><div id='allsearch-error' class='messages error'><h2 class='element-invisible'>Error message</h2>"+Drupal.settings.allsearch_block.all_search_error+"</div></div>").insertBefore('.pane-allsearch-block-allsearch-search');
     		}
     		
 				return false;
