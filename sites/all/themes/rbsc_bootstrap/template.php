0230a6627182ee143919c111f8ea996e4abee7c2
Shaun Ellis <shaun@sdellis.com>
2014-07-29 15:12:47 -0400

    added main nav grouping headers and dividers

--- sites/all/themes/rbsc_bootstrap/template.php
+++ sites/all/themes/rbsc_bootstrap/template.php
@@ -4,3 +4,51 @@
  * @file
  * template.php
  */
+
+/**
+ * Implements hook_menu_link().
+ * Description: Add divisions (---) or headers (_header-string) to main nav
+ */
+
+function rbsc_bootstrap_menu_link(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    // Prevent dropdown functions from being added to management menu so it
+    // does not affect the navbar module.
+    if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
+      $sub_menu = drupal_render($element['#below']);
+    }
+    elseif ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] == 1)) {
+      // Add our own wrapper.
+      unset($element['#below']['#theme_wrappers']);
+      $sub_menu = '<ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
+      // Generate as standard dropdown.
+      $element['#title'] .= ' <span class="caret"></span>';
+      $element['#attributes']['class'][] = 'dropdown';
+      $element['#localized_options']['html'] = TRUE;
+
+      // Set dropdown trigger element to # to prevent inadvertant page loading
+      // when a submenu link is clicked.
+      $element['#localized_options']['attributes']['data-target'] = '#';
+      $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
+      $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
+    }
+  }
+  // On primary navigation menu, class 'active' is not set on active menu item.
+  // @see https://drupal.org/node/1896674
+  if (($element['#href'] == $_GET['q'] || ($element['#href'] == '<front>' && drupal_is_front_page())) && (empty($element['#localized_options']['language']))) {
+    $element['#attributes']['class'][] = 'active';
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+
+  if ($element['#title'] == "---"){
+  	return '<li class="divider"></li>';
+  }elseif ($element['#title'][0] == "_") {
+  	return '<li class="dropdown-header">' . substr($element['#title'], 1) . '</li>';
+  }else{
+  	return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+  }
+
+}
