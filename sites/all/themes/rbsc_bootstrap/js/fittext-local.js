6dc4608eadea6063893a240ff67f9f2581c89967
Shaun Ellis <shaun@sdellis.com>
2014-07-30 10:44:02 -0400

    display photos in splash grid

--- sites/all/themes/rbsc_bootstrap/js/fittext-local.js
+++ sites/all/themes/rbsc_bootstrap/js/fittext-local.js
@@ -4,14 +4,19 @@
 jQuery( document ).ready(function() {
 
 	jQuery(".panels-flexible-region .pane-title").fitText(.6);
+	/*
 	jQuery( ".panels-flexible-region .pane-title" ).each(function() {
   		jQuery( this ).css("background-color", randomColor({hue: 'orange', luminosity: 'dark'}));
 	});
-
+	*/
+	
 	jQuery(".grid-menu-with-sidebar .img-grid .pane-title").fitText(.8);
+
+
 	jQuery( ".grid-menu-with-sidebar .img-grid .pane-title" ).each(function() {
   		jQuery( this ).css("background-color", randomColor({hue: 'blue'}));
 	});
+
 
 	jQuery( ".field-type-taxonomy-term-reference .field-item a").wrapInner( "<span class='label label-primary'></span>");;
 
