49ce87a56837494396660fd24fd2344ead3e376e
Shaun Ellis <shaun@sdellis.com>
2014-07-30 14:41:36 -0400

    updates to front page styles

--- sites/all/themes/rbsc_bootstrap/js/fittext-local.js
+++ sites/all/themes/rbsc_bootstrap/js/fittext-local.js
@@ -3,7 +3,7 @@
 
 jQuery( document ).ready(function() {
 
-	jQuery(".panels-flexible-region .pane-title").fitText(.6);
+	jQuery(".img-grid .pane-title").fitText(.6);
 	/*
 	jQuery( ".panels-flexible-region .pane-title" ).each(function() {
   		jQuery( this ).css("background-color", randomColor({hue: 'orange', luminosity: 'dark'}));
