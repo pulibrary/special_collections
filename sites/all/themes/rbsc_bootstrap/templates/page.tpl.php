5e8fb0acb1adb7b48fb5d03d18175dfe0b5dc93a
Shaun Ellis <shaun@sdellis.com>
2014-09-22 15:57:22 -0400

    tweaking side menu styles

--- sites/all/themes/rbsc_bootstrap/templates/page.tpl.php
+++ sites/all/themes/rbsc_bootstrap/templates/page.tpl.php
@@ -173,7 +173,13 @@
     </header> <!-- /#page-header -->
   
     <div class="row">
-  
+      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+      <?php print render($title_prefix); ?>
+        <?php if (!empty($title)): ?>
+          <h1 class="page-header"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php print $messages; ?>
       <?php if (!empty($page['sidebar_first'])): ?>
         <aside class="col-sm-3" role="complementary">
           <?php print render($page['sidebar_first']); ?>
@@ -184,14 +190,9 @@
         <?php if (!empty($page['highlighted'])): ?>
           <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
         <?php endif; ?>
-        <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+        
         <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if (!empty($title)): ?>
-          <h1 class="page-header"><?php print $title; ?></h1>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php print $messages; ?>
+        
         <?php if (!empty($tabs)): ?>
           <?php print render($tabs); ?>
         <?php endif; ?>
