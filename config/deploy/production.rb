39b1dd3bf2067355f1092cfee20108cdfdbd158c
Carolyn Cole <cac9@princeton.edu>
2020-04-28 09:00:40 -0400

    Updating to point to the correct solr location

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,5 +6,5 @@ server "library-prod1", user: fetch(:user), roles: %w{app drupal_primary}
 server "library-prod3", user: fetch(:user), roles: %w{app drupal_secondary}
 server "library-prod4", user: fetch(:user), roles: %w{app drupal_secondary}
 
-set :search_api_solr_host, 'lib-solr.princeton.edu'
-set :search_api_solr_path, '/solr/rbsc-production'
+set :search_api_solr_host, 'library-solr-prod.princeton.edu'
+set :search_api_solr_path, '/solr/special-collections-prod'
