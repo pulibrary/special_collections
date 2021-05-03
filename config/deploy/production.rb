5668d369ea2f9f8966f56b2bb5105de301c8cf67
Carolyn Cole <cac9@princeton.edu>
2021-05-03 15:17:05 -0400

    Moving to deploy to the new production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,6 @@
 set :files_dir, "special_collections_production_files"
 
-server "library-prod1", user: fetch(:user), roles: %w{app drupal_primary}
-# server "library-prod3", user: fetch(:user), roles: %w{app drupal_secondary}
-# server "library-prod4", user: fetch(:user), roles: %w{app drupal_secondary}
+server "lib-sc-prod1", user: fetch(:user), roles: %w{app drupal_primary}
 
 set :search_api_solr_host, 'library-solr-prod.princeton.edu'
 set :search_api_solr_path, '/solr/special-collections-prod'
