44e7145bce6a2ede8c5c7976e3dd7575b5d5f668
Carolyn Cole <cac9@princeton.edu>
2022-02-03 13:19:16 -0500

    Switching to solr 8 cloud Also updating php to match server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,5 +2,5 @@ set :files_dir, "special_collections_staging_files"
 
 server "lib-sc-staging1", user: fetch(:user), roles: %w{app drupal_primary}
 
-set :search_api_solr_host, 'library-solr-staging.princeton.edu'
+set :search_api_solr_host, 'lib-solr8-staging.princeton.edu'
 set :search_api_solr_path, '/solr/special-collections-staging'
