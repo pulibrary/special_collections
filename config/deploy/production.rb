f0c65dfdffa934cb62f59abcdf9390e005e18979
Kevin Reiss <kevin.reiss@gmail.com>
2023-03-02 11:38:27 -0600

    Run cap db_dump task in app role.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,5 +5,4 @@ server "lib-sc-prod1", user: fetch(:user), roles: %w{app drupal_primary}
 set :search_api_solr_host, 'library-solr-prod.princeton.edu'
 set :search_api_solr_path, '/solr/special-collections-prod'
 
-server "mysql-db-prod1", user: 'pulsys', roles: %w{db}
 set :db_name, "special_collections_prod"
