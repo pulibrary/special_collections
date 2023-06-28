f44246c76ea6e57dcc1714bfaa41e969f633b4de
Francis Kayiwa <kayiwa@users.noreply.github.com>
2023-06-28 11:34:37 -0400

    add new file paths for shared directory (#764)
    we are moving away from SMB to NFS
    this allows us to migrate to the jammy boxes
    part of https://github.com/pulibrary/princeton_ansible/issues/3962
    
    Co-authored-by: Alicia Cozine <acozine@users.noreply.github.com>
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    Co-authored-by: Ryan Laddusaw <rladdusaw@users.noreply.github.com>
    Co-authored-by: Vickie Karasic <vickiekarasic@users.noreply.github.com>

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,9 @@
-set :files_dir, "special_collections_production_files"
+set :files_dir, 'special_collections_production_files'
 
-server "lib-sc-prod1", user: fetch(:user), roles: %w{app drupal_primary}
+server 'lib-sc-prod1', user: fetch(:user), roles: %w[app drupal_primary]
 
+set :drupal_fileshare_mount, '/mnt/diglibdata/drupalweb'
 set :search_api_solr_host, 'library-solr-prod.princeton.edu'
 set :search_api_solr_path, '/solr/special-collections-prod'
 
-set :db_name, "special_collections_prod"
+set :db_name, 'special_collections_prod'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,9 @@
-set :files_dir, "special_collections_staging_files"
+set :files_dir, 'special_collections_staging_files'
 
-server "lib-sc-staging1", user: fetch(:user), roles: %w{app drupal_primary}
+server 'lib-sc-staging1', user: fetch(:user), roles: %w[app drupal_primary]
 
+set :drupal_fileshare_mount, '/mnt/nfs/drupal7'
 set :search_api_solr_host, 'lib-solr8-staging.princeton.edu'
 set :search_api_solr_path, '/solr/special-collections-staging'
 
-set :db_name, "special_collections_staging"
+set :db_name, 'special_collections_staging'
